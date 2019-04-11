0b14280cb53e270b5f79cede6651e9eb7dd2725b
LxMat <38223071+LxMat@users.noreply.github.com>
2019-04-11 19:18:17 +0200

    cardquestions are now responsive

--- src/Game/Card.jsx
+++ src/Game/Card.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 export default function Card(props) {
-  let selected = (props.toggle)?'selected':'unselected';
+  let selected = (props.toggle)?' selected':' unselected';
+  // console.log("style", props.cardStyle)
   return (
     <div>
       <div
-        className={`cardItem ${selected}` }
+        className={props.cardStyle + selected }
         onClick={() => props.onClick()}
-        style={{ backgroundImage: `url(${props.image})` }}
-      />
+        
+      ><img src={props.image}  alt="poster"></img>
+      </div>
     </div>
   )
 }
